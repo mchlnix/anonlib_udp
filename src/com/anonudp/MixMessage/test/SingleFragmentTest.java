a0a45737fcd53136e2bcea9747404eaba9724bb2
Michael Nix <mchl.nix@googlemail.com>
2018-12-18 18:57:41 +0100

    reformats SingleFragmentTest to comply with jUnit

--- src/com/anonudp/MixMessage/test/SingleFragmentTest.java
+++ src/com/anonudp/MixMessage/test/SingleFragmentTest.java
@@ -2,28 +2,24 @@ package com.anonudp.MixMessage.test;
 
 import com.anonudp.MixMessage.Fragment;
 import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class SingleFragmentTest extends TestCase {
+class SingleFragmentTest extends TestCase {
     private Fragment fragment;
 
     private byte[] message_id_bytes = {0x00, 0x03};
     private byte[] payload;
     private byte[] padding_size_bytes;
 
-    public SingleFragmentTest() {
-        super();
-    }
-
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-
+    @BeforeEach
+    protected void setUp() {
         int payload_length = 20;
 
         this.payload = new byte[payload_length];
@@ -34,7 +30,8 @@ public class SingleFragmentTest extends TestCase {
         this.fragment = new Fragment(1234, 0, true, this.payload, Fragment.FRAGMENT_DATA_PAYLOAD);
     }
 
-    public void testMessageId() {
+    @Test
+    void getMessage_id() {
         try {
             assertEquals(Fragment.SINGLE_FRAGMENT_MESSAGE_ID, this.fragment.getMessage_id());
             assertArrayEquals(this.message_id_bytes, Arrays.copyOf(fragment.toBytes(), this.message_id_bytes.length));
@@ -43,46 +40,49 @@ public class SingleFragmentTest extends TestCase {
         }
     }
 
-    public void testFragmentNumber() {
+    @Test
+    void getFragment_number() {
         assertEquals(Fragment.SINGLE_FRAGMENT_FRAGMENT_NUMBER, this.fragment.getFragment_number());
     }
 
-    public void testPayloadLength() {
+    @Test
+    void isLast_fragment() {
+        assertTrue(this.fragment.isLast_fragment());
+    }
+
+    @Test
+    void getPayload() {
         assertEquals(this.payload.length, this.fragment.getPayload().length);
 
         assertArrayEquals(this.payload, this.fragment.getPayload());
     }
 
-    public void testPaddingBytes() {
-        assertArrayEquals(this.padding_size_bytes, this.fragment.getPadding_bytes());
-    }
-
-    public void testPaddingLength() {
+    @Test
+    void getPadding_length() {
         int expected = Fragment.FRAGMENT_LENGTH - Fragment.FRAGMENT_ID_SIZE -
                 this.fragment.getPadding_bytes().length - this.fragment.getPayload().length;
 
         assertEquals(expected, this.fragment.getPadding_length());
     }
 
-    public void testToBytes() {
+    @Test
+    void getPadding_bytes() {
+        assertArrayEquals(this.padding_size_bytes, this.fragment.getPadding_bytes());
+    }
+
+    @Test
+    void toBytes() {
         try (FileOutputStream fos = new FileOutputStream("/home/michael/Schreibtisch/packet2.bytes")) {
             assertEquals(Fragment.FRAGMENT_LENGTH, this.fragment.toBytes().length);
+
+            Fragment copy = new Fragment(this.fragment.toBytes());
+
+            assertArrayEquals(this.fragment.toBytes(), copy.toBytes());
 
             fos.write(this.fragment.toBytes());
         } catch (IOException e) {
             e.printStackTrace();
             fail();
         }
-    }
-
-    public void testByteArrayConstructor() {
-        try {
-            Fragment copy = new Fragment(this.fragment.toBytes());
-
-            assertArrayEquals(this.fragment.toBytes(), copy.toBytes());
-        } catch (IOException e) {
-            fail();
-        }
-
     }
 }
