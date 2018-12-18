f99d68a744af5c2ccef0d688ec1619c8b001679c
Michael Nix <mchl.nix@googlemail.com>
2018-12-19 00:13:23 +0100

    adds FragmentPool class

---
+++ src/com/anonudp/MixMessage/FragmentPool.java
@@ -0,0 +1,57 @@
+package com.anonudp.MixMessage;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class FragmentPool implements Iterator<byte[]> {
+    private HashMap<Integer, Message> messages;
+
+    FragmentPool()
+    {
+        this.messages = new HashMap<>();
+    }
+
+    public void addFragment(Fragment fragment)
+    {
+        int message_id = fragment.getMessage_id();
+
+        if (! this.messages.containsKey(message_id))
+            this.messages.put(message_id, new Message(message_id));
+
+        this.messages.get(message_id).addFragment(fragment);
+    }
+
+    @Override
+    public boolean hasNext() {
+        for (int message_id : this.messages.keySet())
+        {
+            if (this.messages.get(message_id).isDone())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public byte[] next() {
+        for (int message_id : this.messages.keySet())
+        {
+            if (this.messages.get(message_id).isDone())
+            {
+                return this.messages.remove(message_id).getPayload();
+            }
+        }
+
+        throw new NoCompleteMessagesException();
+    }
+
+    public class NoCompleteMessagesException extends NoSuchElementException
+    {
+        NoCompleteMessagesException() {
+            super();
+        }
+    }
+}
