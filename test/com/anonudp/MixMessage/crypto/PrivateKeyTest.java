b81c9fca1f050853a81ad41e078bb00669fc0073
Michael Nix <mchl.nix@googlemail.com>
2018-12-20 17:33:26 +0100

    adds PrivateKey tests

---
+++ test/com/anonudp/MixMessage/crypto/PrivateKeyTest.java
@@ -0,0 +1,29 @@
+package com.anonudp.MixMessage.crypto;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrivateKeyTest extends TestCase {
+
+    @DisplayName("Random Key-Generation")
+    @Test
+    void randomKeys()
+    {
+        int iterations = 100;
+
+        PrivateKey previousKey = new PrivateKey();
+        PrivateKey thisKey = null;
+
+        for (int i = 0; i < iterations; ++i)
+        {
+            thisKey = new PrivateKey();
+
+            assertNotEquals(previousKey, thisKey);
+
+            previousKey = thisKey;
+        }
+    }
+}
