36bdff2925a648a54c4b0b7c429f0b8f3a02ed72
Michael Nix <mchl.nix@googlemail.com>
2018-12-20 17:07:38 +0100

    adds random byte generation to util class

--- src/com/anonudp/MixMessage/Util.java
+++ src/com/anonudp/MixMessage/Util.java
@@ -1,7 +1,20 @@
 package com.anonudp.MixMessage;
 
-class Util {
+import java.security.SecureRandom;
+
+public class Util {
+    private static final SecureRandom random = new SecureRandom();
+
     static double log2(double num) {
         return (Math.log(num) / Math.log(2));
+    }
+
+    public static byte[] randomBytes(int numberOfBytes)
+    {
+        byte[] randomBytes = new byte[numberOfBytes];
+
+        Util.random.nextBytes(randomBytes);
+
+        return randomBytes;
     }
 }
