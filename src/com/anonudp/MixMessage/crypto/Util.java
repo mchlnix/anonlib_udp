6fa7c1d141d3655d00a6e1f3bcefdd33d3b4263f
Michael Nix <mchl.nix@googlemail.com>
2018-12-23 23:58:02 +0100

    adds GCM cipher creation

--- src/com/anonudp/MixMessage/crypto/Util.java
+++ src/com/anonudp/MixMessage/crypto/Util.java
@@ -2,6 +2,7 @@ package com.anonudp.MixMessage.crypto;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidAlgorithmParameterException;
@@ -10,6 +11,8 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 
 public class Util {
+    private static int MAC_SIZE = 16;
+
     public static Cipher createCTRCipher(byte[] symmetricKey, byte[] iv, int mode) throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
         Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding", "BC");
 
@@ -23,5 +26,20 @@ public class Util {
 
     public static Cipher createCTRCipher(byte[] symmetricKey, int mode) throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
         return createCTRCipher(symmetricKey, new byte[EccGroup713.symmetricKeyLength], mode);
+    }
+
+    public static Cipher createGCM(byte[] symmetricKey, byte[] iv, int mode) throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, InvalidKeyException {
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+
+        SecretKeySpec keySpec = new SecretKeySpec(symmetricKey, "AES");
+        GCMParameterSpec gcmSpec = new GCMParameterSpec(MAC_SIZE * 8, iv);
+
+        cipher.init(mode, keySpec, gcmSpec);
+
+        return cipher;
+    }
+
+    public static Cipher createGCM(byte[] symmetricKey, int mode) throws NoSuchProviderException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        return createGCM(symmetricKey, new byte[EccGroup713.symmetricKeyLength], mode);
     }
 }
