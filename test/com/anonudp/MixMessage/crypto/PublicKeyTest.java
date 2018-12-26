21661e5caf6bba4c9fbafaa96abc67df6fd1d275
Michael Nix <mchl.nix@googlemail.com>
2018-12-26 17:24:11 +0100

    adds equality Public Key test

--- test/com/anonudp/MixMessage/crypto/PublicKeyTest.java
+++ test/com/anonudp/MixMessage/crypto/PublicKeyTest.java
@@ -4,6 +4,8 @@ import junit.framework.TestCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 class PublicKeyTest extends TestCase {
 
     @DisplayName("Import and export are compatible")
@@ -14,5 +16,24 @@ class PublicKeyTest extends TestCase {
         PublicKey key = new PublicKey(random);
 
         assertEquals(key, PublicKey.fromBytes(key.toBytes()));
+    }
+
+    @DisplayName("Tests equality")
+    @Test
+    void equals()
+    {
+        PrivateKey random1 = new PrivateKey();
+        PrivateKey random2 = new PrivateKey();
+
+        PublicKey pub1 = new PublicKey(random1);
+        PublicKey pub2 = new PublicKey(random2);
+
+        assertNotEquals(pub1, pub2);
+        assertEquals(pub1, pub1);
+        assertEquals(pub2, pub2);
+
+        PublicKey pub3 = new PublicKey(random2);
+        assertNotEquals(pub1, pub3);
+        assertEquals(pub2, pub3);
     }
 }
