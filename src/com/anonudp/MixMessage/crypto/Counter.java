34d4ee411d580ad505891a52352611f475db9741
Michael Nix <mchl.nix@googlemail.com>
2019-01-04 02:37:16 +0100

    make Counter 4 Bytes

--- src/com/anonudp/MixMessage/crypto/Counter.java
+++ src/com/anonudp/MixMessage/crypto/Counter.java
@@ -2,15 +2,9 @@ package com.anonudp.MixMessage.crypto;
 
 import java.nio.ByteBuffer;
 
-/*
-TODO: Only use 4 Bytes as CTR_PREFIX_SIZE, or switch to BigInteger
- */
-
-public class Counter {
-    public static final int CTR_PREFIX_SIZE = 8;
-    private static final int CTR_PREFIX_OFFSET = CTR_PREFIX_SIZE - Integer.BYTES;
+public class Counter implements Comparable<Counter> {
+    public static final int CTR_PREFIX_SIZE = Integer.BYTES;
     private static final int IV_LENGTH = 16;
-    private static final int IV_OFFSET = CTR_PREFIX_OFFSET;
 
     private int currentValue;
 
@@ -26,8 +20,7 @@ public class Counter {
 
     public Counter(byte[] prefix)
     {
-        // todo this only works for 4 Byte numbers?
-        this(ByteBuffer.wrap(prefix).getInt(CTR_PREFIX_OFFSET));
+        this(ByteBuffer.wrap(prefix).getInt());
     }
 
     public int asInt()
@@ -43,7 +36,7 @@ public class Counter {
     public byte[] asBytes()
     {
         ByteBuffer buffer = ByteBuffer.allocate(CTR_PREFIX_SIZE);
-        buffer.putInt(CTR_PREFIX_OFFSET, this.currentValue);
+        buffer.putInt(this.currentValue);
 
         return buffer.array();
     }
@@ -51,7 +44,7 @@ public class Counter {
     public byte[] asIV()
     {
         ByteBuffer buffer = ByteBuffer.allocate(IV_LENGTH);
-        buffer.putInt(IV_OFFSET, this.currentValue);
+        buffer.putInt(this.currentValue);
 
         return buffer.array();
     }
@@ -64,5 +57,10 @@ public class Counter {
         }
 
         return super.equals(obj);
+    }
+
+    @Override
+    public int compareTo(Counter counter) {
+        return 0;
     }
 }
